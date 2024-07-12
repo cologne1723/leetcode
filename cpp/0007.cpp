e4da012de7ca3bf47211ade957e96f0b2fb1aa09
cologne1723 <corrupted.cologne@gmail.com>
2024-07-12 23:24:42 +0900

    0007

---
+++ cpp/0007.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        unsigned ux = x;
+        unsigned z = x > 0 ? ux : -ux;
+        unsigned u = 0;
+        while(z)
+        {
+            if(u > (1u<<31)/10) return 0;
+            u = u * 10;
+            if(u > (1u<<31) - z % 10) return 0;
+            u += z % 10;
+            z /= 10;
+        }
+        return x < 0 ? -u : u;
+    }
+};
