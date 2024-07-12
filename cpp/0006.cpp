c3729a7c0da5ba236ae352c56feeb3e59a52db44
cologne1723 <corrupted.cologne@gmail.com>
2024-07-12 23:19:37 +0900

    0006

---
+++ cpp/0006.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1) return s;
+
+        vector<string> a(numRows);
+        int x = 0, dx = 1;
+        for(char c: s)
+        {
+            a[x].push_back(c);
+            if(x+dx < 0 || x + dx >= numRows)
+                dx *= -1;
+            x += dx;
+        }
+        string ans;
+        for(auto s: a) ans += s;
+        return ans;
+    }
+};
